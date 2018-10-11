fe7feca3fa1fcb20a3d1161e37ac15af01518e61
kduane <karl.duane@gmail.com>
2018-10-11 18:28:44 -0400

    refactored Factory to use sequences

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :recipe do
-    name { "Green Eggs & Ham" }
-    description { "Would you eat them in a box?  Would you eat them with a fox?" }
-    instructions { "Eggs + Ham + Food coloring" }
-    servings { 4 }
-    cooking_time { 45 }
+    sequence(:name) { |n| "Brussels Sprouts with Bacon #{n}" }
+    description "The best side-dish, ever."
+    instructions "Brussels + Bacon + Heat"
+    servings 4
+    cooking_time 45
   end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -3,15 +3,18 @@ require 'rails_helper'
 RSpec.describe Recipe, type: :model do
   describe ".quick_recipes" do
     it "returns recipes with a cooking time less than 30 min" do
-      first_quick_recipe = FactoryBot.create(:recipe, cooking_time: 5)
-      second_quick_recipe = FactoryBot.create(:recipe, cooking_time: 4)
-      long_recipe = FactoryBot.create(:recipe, cooking_time: 240)
+      quick_recipes = create_list(:recipe, 2, cooking_time: 5)
+      slow_recipes = create_list(:recipe, 3, cooking_time: 240)
 
       results = Recipe.quick_recipes
 
-      expect(results).to include(first_quick_recipe)
-      expect(results).to include(second_quick_recipe)
-      expect(results).to_not include(long_recipe)
+      quick_recipes.each do |quick_recipe|
+        expect(results).to include(quick_recipe)
+      end
+
+      slow_recipes.each do |slow_recipe|
+        expect(results).to_not include(slow_recipe)
+      end
     end
   end
 end
